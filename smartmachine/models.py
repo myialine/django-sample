a85aca02011330a1d170fde7b8a5d47d133546da
Aline Cavalcante <Aline-Cavalcante@live.nmit.ac.nz>
2020-07-31 18:09:43 +1200

    Migration created

--- smartmachine/models.py
+++ smartmachine/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class Machine(models.Model):
+    STATUS = [('Y', 'Active'), ('N', 'Not Active')]
+    name = models.CharField(max_length=100)
+    current_status = models.CharField(max_length=1, choices=STATUS, default='N')
 
-# Create your models here.
+
