ce7e7705ea15b76cdb6fa42a217ee9a7ac700072
Aline Cavalcante <Aline-Cavalcante@live.nmit.ac.nz>
2020-08-01 19:44:03 +1200

    Updated README TODOs

--- smartmachine/views.py
+++ smartmachine/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
-
-from .models import Machine
 
 
 def home(request):
-    try:
-        machines = Machine.objects.all()
-    except Machine.DoesNotExist:
-        raise Http404('Machines not found')
-    return render(request, 'home.html', {'machines': machines, })
+    return render(request, 'home.html')
